187d3d87148e16afc89a3ecbbcbd3005d2882c0c
ye0ngeun <duddms2415@naver.com>
2025-11-21 19:20:36 +0900

    chore: System.out.println을 SLF4J 로깅으로 변경
    - ExternalTransferPortImpl에 @Slf4j 어노테이션 추가
    - System.out.println을 log.info()로 변경
    - 파라미터 바인딩을 활용한 효율적인 로깅
    - external-system 모듈에 Lombok 의존성 추가
    - 운영 환경에 적합한 로깅 프레임워크 적용

--- external-system/src/main/java/com/bank/external/adapter/ExternalTransferPortImpl.java
+++ external-system/src/main/java/com/bank/external/adapter/ExternalTransferPortImpl.java
@@ -1,14 +1,16 @@
 package com.bank.external.adapter;
 
 import com.bank.common.port.ExternalTransferPort;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class ExternalTransferPortImpl implements ExternalTransferPort {
     
     @Override
     public String send(String message) {
-        System.out.println("External system sending: " + message);
+        log.info("External system sending: {}", message);
         return "external-ok";
     }
 } 
