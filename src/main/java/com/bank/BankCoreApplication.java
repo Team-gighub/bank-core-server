e8790afaa5afcf91dcaf5a828b82b94fb6773caa
ye0ngeun <duddms2415@naver.com>
2025-11-21 19:13:43 +0900

    refactor: BankCoreApplication에서 불필요한 scanBasePackages 제거
    - @SpringBootApplication은 기본적으로 해당 클래스 패키지와 하위 패키지를 자동 스캔
    - com.bank.deposit, com.bank.external은 com.bank의 하위 패키지이므로 명시 불필요
    - 코드 간결성 향상 및 Spring Boot의 관례 준수

--- src/main/java/com/bank/BankCoreApplication.java
+++ src/main/java/com/bank/BankCoreApplication.java
@@ -3,11 +3,7 @@ package com.bank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {
-		"com.bank",
-		"com.bank.deposit",
-		"com.bank.external"
-})
+@SpringBootApplication
 public class BankCoreApplication {
 
 	public static void main(String[] args) {
