65d1cd47626129dc8543b9c1973ca80407a6da68
SooY2 <ssy67366736@gmail.com>
2025-11-28 19:04:12 +0900

    feat: AccountRepository accountNumber으로 데이터 조회

--- deposit-system/src/main/java/com/bank/deposit/repository/AccountRepository.java
+++ deposit-system/src/main/java/com/bank/deposit/repository/AccountRepository.java
@@ -4,8 +4,11 @@ import com.bank.deposit.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// JpaRepository를 상속받아 기본 CRUD 메서드를 제공받습니다.
+import java.util.Optional;
+
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
-    // 필요한 경우 추가적인 조회 메서드를 정의할 수 있습니다.
+
+    Optional<Account> findByAccountNumber(String accountNumber);
 }
