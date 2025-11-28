4ad5ad9b6691000a8a648c213965356c4ed69464
youm0524 <juliet0524@naver.com>
2025-11-28 14:51:15 +0900

    fix: repository 변경

--- deposit-system/src/main/java/com/bank/deposit/repository/AccountRepository.java
+++ deposit-system/src/main/java/com/bank/deposit/repository/AccountRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 
 // JpaRepository를 상속받아 기본 CRUD 메서드를 제공받습니다.
 @Repository
-public interface AccountRepository extends JpaRepository<Account, String> {
+public interface AccountRepository extends JpaRepository<Account, Integer> {
     // 필요한 경우 추가적인 조회 메서드를 정의할 수 있습니다.
 }
--- deposit-system/src/main/java/com/bank/deposit/repository/LedgerRepository.java
+++ deposit-system/src/main/java/com/bank/deposit/repository/LedgerRepository.java
@@ -2,8 +2,9 @@ package com.bank.deposit.repository;
 
 import com.bank.deposit.domain.Ledger;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LedgerRepository extends JpaRepository<Ledger, String> {
+public interface LedgerRepository extends JpaRepository<Ledger, Integer> {
 }
