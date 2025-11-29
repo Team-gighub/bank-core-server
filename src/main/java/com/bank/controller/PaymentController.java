a389789928003d5b6a57cb8bbb49d74ce8c12860
youm0524 <juliet0524@naver.com>
2025-11-29 11:38:10 +0900

    feat: 승인 컨트롤러

---
+++ src/main/java/com/bank/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package com.bank.controller;
+
+import com.bank.common.response.ApiResponse;
+import com.bank.deposit.dto.ApprovalRequest;
+import com.bank.deposit.dto.ApprovalResponse;
+import com.bank.deposit.service.ApprovalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/payment")
+@RestController
+@RequiredArgsConstructor
+public class PaymentController {
+    private final ApprovalService approveService;
+
+    @PostMapping("/approval")
+    public ApprovalResponse createEscrow(@RequestBody ApprovalRequest request)
+    {
+        ApprovalResponse response = approveService.recodeLedger(request);
+        return response;
+    }
+}
