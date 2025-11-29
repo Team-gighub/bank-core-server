44e1c822014346262c0a45a291005293e9bc6dab
SooY2 <ssy67366736@gmail.com>
2025-11-29 11:41:11 +0900

    feat: [대외계] 금결원 mock클래스에서 결과 불러오도록

--- external-system/src/main/java/com/bank/external/adapter/ExternalValidatePortImpl.java
+++ external-system/src/main/java/com/bank/external/adapter/ExternalValidatePortImpl.java
@@ -1,6 +1,7 @@
 package com.bank.external.adapter;
 
 import com.bank.common.port.ExternalValidatePort;
+import com.bank.external.kftc.MockOpenBanking;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +10,13 @@ import java.math.BigDecimal;
 @Slf4j
 @Component
 public class ExternalValidatePortImpl implements ExternalValidatePort {
+
+    private MockOpenBanking openBanking;
+
     @Override
     public boolean isWithdrawalPossible(String bankCode, String accountNo, BigDecimal amount) {
         log.info("외부 계좌 출금 가능 여부 조회 → {}, {}, {}", bankCode, accountNo, amount);
-
-        return true;
+        return openBanking.isAccountValid(bankCode, accountNo, amount);
 
     }
 }
