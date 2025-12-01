d0b0094128d809451deb6da8a25c1e0f3b93bf83
youm0524 <juliet0524@naver.com>
2025-12-01 10:24:40 +0900

    feat: 대외계 호출 로직 추가

---
+++ external-system/src/main/java/com/bank/external/adapter/ExternalWithdrawPortImpl.java
@@ -0,0 +1,22 @@
+package com.bank.external.adapter;
+
+import com.bank.common.port.ExternalWithdrawPort;
+import com.bank.external.kftc.MockCommonBanking;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ExternalWithdrawPortImpl implements ExternalWithdrawPort {
+    private final MockCommonBanking commonBanking;
+    @Override
+    public boolean isWithdrawalSuccess(String bankCode, String accountNo, BigDecimal amount) {
+        log.info("외부 계좌 출금 성공 여부 조회 → {}, {}, {}", bankCode, accountNo, amount);
+        return commonBanking.isWithdrawSuccess(bankCode, accountNo, amount);
+
+    }
+}
